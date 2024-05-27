dc0a94eeee3cb4fbfbf3c1030fe0a99f5d55fe40
Kramer <kramer@facialstats.com>
2024-05-27 18:48:10 -0400

    jump

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class BasicMovement : MonoBehaviour
 {
     [SerializeField] private CharacterController controller;
-    [SerializeField] private float speed = 10f, gravity = -9.8f/2;
+    [SerializeField] private float speed = 10f, gravity = -9.8f,checkLength = 0.1f,jumpHeight =2f;
 
     private Vector3 velocity;
 
     [SerializeField] private Transform groundCheck;
-    [SerializeField] private float checkLength = 0.1f;
     [SerializeField] private LayerMask groundMask;
     bool isGround;
     // Update is called once per frame
@@ -25,16 +24,30 @@ public class BasicMovement : MonoBehaviour
 
         controller.Move(move *speed* Time.deltaTime);//basic movement
 
+        if (Input.GetButtonDown("Jump") && isGround)//jump
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
 
     }
     private void FixedUpdate()
     {
-        velocity.y += gravity * Time.deltaTime;
+
         if (isGround && velocity.y < 0)
         {
-            velocity.y = -0.3f;
+            velocity.y = -0.1f;
         }
-        controller.Move(velocity);//fall
+        if (velocity.y>0)
+        {
+            velocity.y += gravity * Time.deltaTime / 2;
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime/4;
+        }
+        
+        controller.Move(velocity/2);//fall
+
     }
     private void OnDrawGizmos()
     {
